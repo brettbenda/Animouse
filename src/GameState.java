a854023b9bc1bf01918674d46011f9c341333864
Michael Wulber <mwulber2015@ufl.edu>
2018-03-13 18:32:50 -0400

    added player classes and fleshed out GameState

--- src/GameState.java
+++ src/GameState.java
@@ -1,7 +1,23 @@
 public class GameState {
+    // current level data
+    private Level level;
+
+    // currently selected player: T = 0, J = 1
+    private int selectedPlayer;
+
+    // Tim and Jack
+    private Tim tim;
+    private Jack jack;
 
     // constructor
-    public GameState() {
-        // ...
+    public GameState(Level level) {
+        this.level = level;
+        this.selectedPlayer = 0;
+        this.tim = new Tim();
+        this.jack = new Jack();
+    }
+
+    public void loadLevel(Level level) {
+        this.level = level;
     }
 }
