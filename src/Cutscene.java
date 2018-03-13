f5c3cf33f6da88b1f8a16e340c75959a73d5f622
GwenYu <gwenyu517@gmail.com>
2018-03-13 11:21:02 -0400

    Attempt to push.

--- src/Cutscene.java
+++ src/Cutscene.java
@@ -2,7 +2,7 @@ import java.awt.image.BufferedImage;
 
 public class Cutscene {
 
-    BufferedImage imageLayer;
+
     // constructor
     public Cutscene() {
         // ...
--- src/Level.java
+++ src/Level.java
@@ -1,9 +1,15 @@
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 public class Level {
 
     // constructor
-    BufferedImage yo;
+    BufferedImage imageLayer;
+    BufferedImage bitmap;           // a.k.a. effectLayer
+
+    Point2D.Float[] hookablePoints;
+    Point2D.Float posTim;
+
     public Level() {
         // ...
     }
