61ae5f89c59b5ee6a5bbeb87e99a459f9f2e0ea0
GwenYu <gwenyu517@gmail.com>
2018-03-14 19:17:42 -0400

    Working on VisualAssets. Nothing actually works yet.

--- src/Cutscene.java
+++ src/Cutscene.java
@@ -1,7 +1,10 @@
 import java.awt.image.BufferedImage;
 
 public class Cutscene {
-
+    BufferedImage[] frames;     // a.k.a. scenes
+    // Sounds
+    BufferedImage currentFrame;     // a.k.a. currentScene
+    int index;
 
     // constructor
     public Cutscene() {
--- src/VisualAssets.java
+++ src/VisualAssets.java
@@ -1,8 +1,32 @@
+import java.awt.image.BufferedImage;
 
 public class VisualAssets {
 
+    BufferedImage TIM;
+    BufferedImage[] TIM_JUMPING_ANIM;
+    BufferedImage[] TIM_WALKING_ANIM;
+    BufferedImage[] TIM_IDLE_ANIM;
+
+    BufferedImage JACK;
+    BufferedImage[] JACK_JUMPING_ANIM;
+    BufferedImage[] JACK_WALKING_ANIM;
+    BufferedImage[] JACK_IDLE_ANIM;
+
+    BufferedImage[] LEVELX_BACKGROUND;      // a.k.a. LEVELX_ART_LAYER
+    BufferedImage[] LEVELX_BITMAP;          // a.k.a. LEVELX_ZONE_LAYER
+    BufferedImage[][] LEVELX_CUTSCENES;
+
+    BufferedImage[][] INTERMISSION_CUTSCENES;
+
     public VisualAssets(){
-        
+        // load all assets
+       loadTimArt();
     }
+
+    private void loadTimArt(){
+        TIM = ImageLoader.loadImage("/VisualAssets/Tim/Tim.png");
+
+    }
+
 }
 
